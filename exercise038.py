65f19ba347786ce06b65712e2bc6931a78368b0f
Daniel Mafra <danielmafradev@gmail.com>
2021-07-27 17:13:01 -0300

    Add exercise038

---
+++ exercise038.py
@@ -0,0 +1,9 @@
+n1 = int(input('First number: '))
+n2 = int(input('Second number: '))
+
+if n1 > n2:
+    print('The first number is higher')
+elif n2 > n1:
+    print('The second number is higher')
+else:
+    print('The two numbers are the same')

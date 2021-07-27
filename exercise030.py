9609b7d835efe855246c04adcd3c67769da66b69
Daniel Mafra <danielmafradev@gmail.com>
2021-07-27 12:04:35 -0300

    Add exercise030

---
+++ exercise030.py
@@ -0,0 +1,7 @@
+number = int(input('Tell me any number: '))
+result = number % 2
+
+if result == 0:
+    print('The number {} is EVEN'.format(number))
+else:
+    print('The number {} is ODD'.format(number))

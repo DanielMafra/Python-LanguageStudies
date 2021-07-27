f2e5cadc341363757ca4ab63daaed0f0a924123c
Daniel Mafra <danielmafradev@gmail.com>
2021-07-27 18:05:21 -0300

    Add exercise042

---
+++ exercise042.py
@@ -0,0 +1,18 @@
+print('-=' * 20)
+print('Triangle Analyzer')
+print('-=' * 20)
+
+s1 = float(input('First segment: '))
+s2 = float(input('Second segment: '))
+s3 = float(input('Third segment: '))
+
+if s1 < s2 + s3 and s2 < s1 + s3 and s3 < s1 + s2:
+    print('The above segments can form a triangle ', end='')
+    if s1 == s2 == s3:
+        print('equilateral')
+    elif s1 != s2 != s3 != s1:
+        print('scalene')
+    else:
+        print('isosceles')
+else:
+    print('The above segments cannot form a triangle!')

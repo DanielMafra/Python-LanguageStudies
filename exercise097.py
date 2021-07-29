f15aca70fad9081ef9191b29691cb90fe4692238
Daniel Mafra <danielmafradev@gmail.com>
2021-07-29 16:55:34 -0300

    Add exercise097

---
+++ exercise097.py
@@ -0,0 +1,10 @@
+def write(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f' {msg}')
+    print('~' * tam)
+
+
+write('Gustavo Guanabara')
+write('Python course on Youtube')
+write('CeV')
